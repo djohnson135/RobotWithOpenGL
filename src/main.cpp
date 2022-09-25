1f66dbccbfdceee9cc1ec43cff2d8dfb39553b22
djohnson135 <Dathanljohnson135@gmail.com>
2022-09-24 20:22:11 -0500

    Adding recurive draw

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ glm::vec3 up(0.0f, 1.0f, 0.0f);
 Program program;
 MatrixStack modelViewProjectionMatrix;
 
+
 // Draw cube on screen
 void DrawCube(glm::mat4& modelViewProjectionMatrix)
 {
@@ -40,16 +41,16 @@ private:
 	glm::vec3 translationWrtJoint;
 	glm::vec3 scalingFactors;
 	std::string identity;
-	glm::vec3 jointPos;
+	//glm::vec3 jointPos;
+	//glm::vec3 attatchPoint;
 
 public:
 	RobotElements* parent;
 	std::vector<RobotElements*> children;
 	RobotElements() {}
-	RobotElements(std::string _identity) : identity(_identity) {}
-	RobotElements(glm::vec3 _jointTranslationWrtParentJoint, glm::vec3 _jointAngle, glm::vec3 _translationWrtJoint, glm::vec3 _scalingFactors, std::string _identity) :
-		jointTranslationWrtParentJoint(_jointTranslationWrtParentJoint), translationWrtJoint(_translationWrtJoint), scalingFactors(_scalingFactors), identity(_identity) {}
-	
+	RobotElements(std::string _identity): identity() {}
+	RobotElements(std::string _identity, glm::vec3 _jointTranslationWrtParentJoint, glm::vec3 _translationWrtJoint, glm::vec3 _scalingFactors, glm::vec3 _jointAngle) : 
+		identity(_identity), jointTranslationWrtParentJoint(_jointTranslationWrtParentJoint), translationWrtJoint(_translationWrtJoint), scalingFactors(_scalingFactors), jointAngle(_jointAngle) {}
 	~RobotElements() 
 	{ 
 		delete parent; 
@@ -68,9 +69,36 @@ public:
 	{
 		//draw itself
 		//for now just display the identity
-		std::cout << this->get_identity() << std::endl;
+		//std::cout << this->get_identity() << std::endl;
+
+		
+		//move componenent to center of coordinate
+		modelViewProjectionMatrix.pushMatrix();
+		//translate
+		modelViewProjectionMatrix.translate(this->translationWrtJoint);
+
+		modelViewProjectionMatrix.rotateX(glm::radians(this->jointAngle.x)); //need to calculate angle?
+		modelViewProjectionMatrix.rotateY(glm::radians(this->jointAngle.y)); //need to calculate angle?
+		modelViewProjectionMatrix.rotateZ(glm::radians(this->jointAngle.z)); //need to calculate angle?
+
+		
+		//scale
+		modelViewProjectionMatrix.scale(this->scalingFactors);
+
+		//translate to parent joint
+		modelViewProjectionMatrix.translate(this->jointTranslationWrtParentJoint);
+		
+		// joint translation/ rotation/ component translation/ scaling
+
+		modelViewProjectionMatrix.print("cube");
+		DrawCube(modelViewProjectionMatrix.topMatrix());
+		
+		
+		//pop
+		modelViewProjectionMatrix.popMatrix();
 
 		//draw children
+
 		for (auto robotElement : this->children) {
 			robotElement->Draw();
 		}
@@ -79,14 +107,31 @@ public:
 
 };
 
+RobotElements* robot = nullptr;
 
-void ConstructRobot()
+
+RobotElements* ConstructRobot()
 {
+
+	//torso vectors
+	glm::vec3 torsoJoint(0.0f, 0.0f, 0.0f);
+	glm::vec3 torsoParentJoint(0.0f, 0.0f, 0.0f);
+	glm::vec3 torsoScalingFactors(1.0f, 1.5f, 1.0f);
+	glm::vec3 torsoJointAngle(0.0f, 0.0f, 0.0f);
+
+	//rightarm vectors
+
+	glm::vec3 rightArmJoint(0.0f, -0.3f, 0.0f);
+	glm::vec3 rightArmParentJoint(3.0f, 0.0f, 0.0f);
+	glm::vec3 rightArmScalingFactors(1.0f, 1.5f, 1.0f);
+	glm::vec3 rightArmJointAngle(0.0f, 0.0f, 0.0f);
+
+
 	//tree with torso as root
-	RobotElements* torsoRoot = new RobotElements("torso");
+	RobotElements* torsoRoot = new RobotElements("torso", torsoParentJoint, torsoJoint, torsoScalingFactors, torsoJointAngle);
 	torsoRoot->parent = nullptr; //root has no parent
 	torsoRoot->children.push_back((RobotElements*) new RobotElements("left arm")); //leftarm
-	torsoRoot->children.push_back((RobotElements*) new RobotElements("right arm")); //rightarm
+	torsoRoot->children.push_back((RobotElements*) new RobotElements("right arm", rightArmJoint, rightArmParentJoint, rightArmScalingFactors, rightArmJointAngle)); //rightarm
 	torsoRoot->children.push_back((RobotElements*) new RobotElements("left leg")); //leftleg
 	torsoRoot->children.push_back((RobotElements*) new RobotElements("right leg")); //rightleg
 	torsoRoot->children.push_back((RobotElements*) new RobotElements("head")); //head
@@ -97,13 +142,16 @@ void ConstructRobot()
 		torsoRoot->children[i]->children.push_back((RobotElements*) new RobotElements(identity));
 	}
 
-
-	//std::unique_ptr<MatrixStack> ptr = std::unique_ptr<MatrixStack>(&modelViewProjectionMatrix);
 	torsoRoot->Draw();
+
+	return torsoRoot;
 
 }
 
-
+//transformations consists of 3 parts
+//object positioning in world
+//world to camera
+//projection matrix
 
 void Display()
 {	
@@ -111,6 +159,7 @@ void Display()
 
 	modelViewProjectionMatrix.loadIdentity();
 	modelViewProjectionMatrix.pushMatrix();
+	//modelViewProjectionMatrix.print("Identity");
 
 	// Setting the view and Projection matrices
 	int width, height;
@@ -119,12 +168,14 @@ void Display()
 	modelViewProjectionMatrix.LookAt(eye, center, up);
 	
 	// Model transformation for Cube 1
-	modelViewProjectionMatrix.pushMatrix();
-	modelViewProjectionMatrix.translate(-2.0f, 2.0f, 0.0f);
-	modelViewProjectionMatrix.rotateY(glm::radians(45.0f));
-	modelViewProjectionMatrix.scale(0.8);
-	DrawCube(modelViewProjectionMatrix.topMatrix());
-	modelViewProjectionMatrix.popMatrix();
+	//modelViewProjectionMatrix.pushMatrix();
+	//modelViewProjectionMatrix.translate(-2.0f, 2.0f, 0.0f);
+	//modelViewProjectionMatrix.rotateY(glm::radians(45.0f));
+	//modelViewProjectionMatrix.scale(0.8);
+	//DrawCube(modelViewProjectionMatrix.topMatrix());
+	//modelViewProjectionMatrix.popMatrix();
+
+	robot->Draw();
 	
 	// Model transformation for Cube 2
 	//modelViewProjectionMatrix.pushMatrix();
@@ -254,7 +305,7 @@ void Init()
 	program.SetShadersFileName(vertShaderPath, fragShaderPath);
 	program.Init();
 
-	ConstructRobot();
+	robot = ConstructRobot();
 	CreateCube();
 }
 
